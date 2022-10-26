2047df0feaa76214ceeb9e4a2ad31e24e89f496f
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2022-10-26 16:04:59 +0500

    RoughWork

--- Python/IntroTo_Streamlit.py
+++ Python/IntroTo_Streamlit.py
@@ -1,4 +1,10 @@
-pip install streamlit
+# To Install streamlit into your system: pip install streamlit
+# To test if the installation is working or not : streamlit hello
+
+# To Import streamlit into your system:
+
 import streamlit as st
 
-st.stitle('Hello, welcome to my streamlit app!')
+# To create title in streamlit:
+
+st.title('Hello, welcome to my streamlit app!')
