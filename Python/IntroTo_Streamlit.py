43c2a2a08bbead2f8d504d025283d422060d8802
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2022-10-26 16:53:16 +0500

    RoughWork

--- Python/IntroTo_Streamlit.py
+++ Python/IntroTo_Streamlit.py
@@ -46,3 +46,38 @@ finally:
 
 
   st.write("""rem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maximedoloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantiummodi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!""")
+  
+  # For Input widgets:
+  
+  # To create a check box button:
+  
+  st.checkbox('yes')
+  
+  # To create a simple button:
+  
+  st.button('Click')
+
+  # To create a radio box button:
+  
+  st.radio('Pick your gender',['Male','Female'])
+
+  # To create a select box button:
+  
+  st.selectbox('Pick your gender',['Male','Female'])
+
+  # To create a slider:
+  
+  st.slider('Pick a number', 0,50)
+  
+  # To create a category slider:
+  
+  st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+
+  # To create a drop-down:
+  
+  st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+
+ 
+  
+  
+
